047a0acb656225681557991d18b5730a9d58a6b0
NenadPantelic <nenadpantelickg@gmail.com>
2021-06-13 14:35:30 +0200

    Remove deploy scripts and auth folder

--- scripts/db.sql
+++
@@ -1,5 +0,0 @@
-sudo -u postgres psql postgres -> login u postgres shell
-CREATE USER xenecca_user WITH PASSWORD 'z+/M><@KG(,$:B2XmW:U9>,zps(F4%K_' CREATEDB;
-CREATE DATABASE "xenecca" WITH OWNER "xenecca_user" ENCODING 'UTF8' LC_COLLATE = 'en_US.UTF-8' LC_CTYPE = 'en_US.UTF-8';
-
-service postgresql status - status POSTGRES-a
--- scripts/start_server_app.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-SERVER_IP=195.201.112.234
-LOCATION=~/Documents/binary3/xenecca/xenecca-server-deploy
-ssh root@$SERVER_IP 'bash -s' < $LOCATION/cron-jobs/runapi.sh
-
